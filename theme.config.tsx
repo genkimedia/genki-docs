5b9211256d082f745b1d3d0e29b7fe77a96510fb
Genki <hello@genki.site>
2022-12-06 06:36:34 -0500

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <Image width={130} height={44}src="/img/gdocs-logo.svg" alt="Genki Logo"/>,
+  logo: <Image width={130} height={44}src="/img/genki-logo.svg" alt="Genki Logo"/>,
   project: {
     link: 'https://github.com/genkimedia/',
   },
