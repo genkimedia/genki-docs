d0bdeb950f23ba6183c74e5354f2b85264283b0d
Genki <hello@genki.site>
2022-12-06 17:23:01 -0500

    add docs
    Co-Authored-By: Thian <94427218+thiangm@users.noreply.github.com>

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Head, Html, Main, NextScript, DocumentContext } from "next/document";
+
+import Favicon from '../components/Favicon';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<Record<string, unknown> & {html: string}> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <Favicon/>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
