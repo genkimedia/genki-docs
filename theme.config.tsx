44dfa239c33adf4edeff1b11217d2d32b603e0a0
Genki <hello@genki.site>
2022-12-05 23:25:14 -0500

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Genki &#169; 2022',
   },
 }
 
